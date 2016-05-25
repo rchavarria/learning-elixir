0627d82ba0b0a01355d246c66fbadef10ecde8c0
Rubén Chavarría <txingo@gmail.com>
2016-05-25 22:41:46 +0200

    Improve scheduler with a communication protocol

--- code/round-07/exercise-07-round-07.exs
+++ code/round-07/exercise-07-round-07.exs
@@ -1,12 +1,27 @@
 defmodule WordCountServer do
 
-  def parse(scheduler, dir, file) do
-    path = dir <> "/" <> file
-    contents = path
-      |> File.read!
-    word_count = length(String.split(contents, "cat")) - 1
+  def parse(scheduler, dir) do
+    send scheduler, { :ready, self }
 
-    send scheduler, { file, word_count, self }
+    receive do
+      { :count, file } ->
+        IO.puts "Proceso #{inspect self} contando fichero #{file}"
+
+        word_count = count_words_on(dir, file)
+        send scheduler, { :answer, file, word_count }
+        # sigue escuchando
+        parse(scheduler, dir)
+
+      { :shutdown } ->
+        exit(:normal)
+    end
+  end
+
+  defp count_words_on(dir, file) do
+    path = dir <> "/" <> file
+    contents = path |> File.read!
+    word_count = length(String.split(contents, "cat")) - 1
+    word_count
   end
 
 end
@@ -14,19 +29,31 @@ end
 defmodule Scheduler do
 
   def run(dir) do
-    dir
-    |> File.ls!
-    |> Enum.map(fn(file) -> spawn_link(WordCountServer, :parse, [ self, dir, file ]) end)
-    |> gather_counts
+    files = File.ls!(dir)
+
+    files
+    |> Enum.map(fn(_) -> spawn_link(WordCountServer, :parse, [ self, dir ]) end)
+    |> schedule_processes(files, [])
   end
 
-  defp gather_counts([]), do: nil
-  defp gather_counts(pids) do
+  defp schedule_processes(pids, files, results) do
     receive do
-      { file, word_count, pid } ->
-        IO.puts "File #{file} contains #{word_count} cat words"
-        # recibe el siguiente resultado
-        gather_counts(List.delete(pids, pid))
+      { :ready, server } when length(files) > 0 ->
+        [ next_file | tail ] = files
+        send server, { :count, next_file }
+        schedule_processes(pids, tail, results)
+
+      { :ready, server } ->
+        send server, { :shutdown }
+        if length(pids) > 1 do
+          schedule_processes(List.delete(pids, server), files, results)
+        else
+          results
+        end
+
+      { :answer, file, word_count } ->
+        result = "El fichero #{file} contiene #{word_count} cats"
+        schedule_processes(pids, files, [ result | results ])
     end
   end
 
@@ -36,5 +63,6 @@ end
 # Uso del planificador y del servidor
 #
 target_dir = "target"
-Scheduler.run target_dir
+Scheduler.run(target_dir)
+  |> Enum.each(&IO.puts/1)
 
